69090ac78a68a771dfa439c78d4d93bbe0e0f0bc
Janko Slavič <janko.slavic@gmail.com>
2016-02-07 22:28:47 +0100

    testing the MKL

--- mkl/mkl_test.py
+++ mkl/mkl_test.py
@@ -20,6 +20,8 @@ OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABIL
 WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 POSSIBILITY OF SUCH DAMAGE.
+
+Source: https://dpinte.wordpress.com/2010/01/15/numpy-performance-improvement-with-the-mkl/
 """
 import os
 import sys
@@ -76,11 +78,11 @@ def test_dot():
 
 
 # Test to start. The dict is the value I had with the MKL using EPD 6.0 and without MKL using EPD 5.1
-tests = {test_eigenvalue: (752., 3376.),
-         test_svd: (4608., 15990.),
-         test_inv: (418., 1457.),
-         test_det: (186.0, 400.),
-         test_dot: (666., 2444.)}
+tests = (test_eigenvalue,
+         test_svd,
+         test_inv,
+         test_det,
+         test_dot)
 
 # Setting the following environment variable in the shell executing the script allows
 # you limit the maximal number threads used for computation
@@ -88,18 +90,17 @@ THREADS_LIMIT_ENV = 'OMP_NUM_THREADS'
 
 
 def start_benchmark():
-    print("Benchmark is made against NumPy {0:s} with MKL".format(np.__version__))
     if THREADS_LIMIT_ENV in os.environ:
         print("Maximum number of threads used for computation is : %s" % os.environ[THREADS_LIMIT_ENV])
         print("-" * 80)
         print("Starting timing with numpy %s\nVersion: %s" % (np.__version__, sys.version))
         print("%20s : %10s - %5s / %5s" % ("Function", "Timing [ms]", "MKL", "No MKL"))
 
-    for fun, bench in tests.items():
+    for fun in tests:
         t = timeit.Timer(stmt="%s()" % fun.__name__, setup="from __main__ import %s" % fun.__name__)
         res = t.repeat(repeat=3, number=1)
         timing = 1000.0 * sum(res) / len(res)
-        print("%20s : %7.1f ms - %3.2f / %3.2f" % (fun.__name__, timing, bench[0] / timing, bench[1] / timing))
+        print("%20s : %7.1f ms" % (fun.__name__, timing))
 
 
 if __name__ == '__main__':
